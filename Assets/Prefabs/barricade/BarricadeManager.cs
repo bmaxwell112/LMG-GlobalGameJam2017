4f14be259d8ae21e9fb53e803caff36b2f52e978
Rex Soriano <rex@rex-soriano.com>
2018-01-27 15:10:05 -0500

    Basic Barricade scripts

---
+++ Assets/Prefabs/barricade/BarricadeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarricadeManager : MonoBehaviour {
+    
+    public int health;
+    private BoxCollider2D collisionBox;
+
+    public Sprite[] imageArray;
+
+	// Use this for initialization
+	void Start () {
+        collisionBox = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health == 0)
+        {
+            collisionBox.enabled = false;
+        }else
+        {
+            collisionBox.enabled = true;
+        }
+	}
+
+    public void BuildBarricade()
+    {
+        health++;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+    }
+}
