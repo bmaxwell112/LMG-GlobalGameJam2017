1acb7f2a41a745eee571f84b5b80da9e07e09617
Brett Maxwell <bmaxwell112@gmail.com>
2018-01-27 10:59:12 -0500

    added player with movement and enemy spawner

---
+++ Assets/Prefabs/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float minPos, maxPos, speed;
+
+    // Update is called once per frame
+    void Update () {
+        MovementCheck();
+    }
+
+    void MovementCheck()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= minPos)
+        {
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x <= maxPos)
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("attack");
+        }
+    }
+}
---
+++ Assets/Prefabs/enemies/EnemyAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+	}
+}
