f2ef14d15a1d361f214cdb73b4280a9b117df4d2
Rex Soriano <rex@rex-soriano.com>
2018-01-28 16:51:28 -0500

    commit door sound effects

--- Assets/Prefabs/barricade/BarricadeManager.cs
+++ Assets/Prefabs/barricade/BarricadeManager.cs
@@ -6,21 +6,29 @@ public class BarricadeManager : MonoBehaviour {
     
     public int health;
     public GameObject door;
+    public AudioClip[] knocks;
+    public AudioClip doorOpen;
+
     private BoxCollider2D collisionBox;    
     private GameObject doorImage;
+    private AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         collisionBox = GetComponent<BoxCollider2D>();
         doorImage = transform.GetChild(0).gameObject;
+        audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(health <= 0)
+		if(health <= 0 && doorImage.GetComponent<SpriteRenderer>().enabled == false)
         {
+            PlayAudio(doorOpen);
             doorImage.GetComponent<SpriteRenderer>().enabled = true;
-        }else
+        }
+        if (health > 0)
         {
             doorImage.GetComponent<SpriteRenderer>().enabled = false;
         }
@@ -33,10 +41,18 @@ public class BarricadeManager : MonoBehaviour {
 
     public void TakeDamage(int damage)
     {
+        int rand = UnityEngine.Random.Range(0, 2);
+        PlayAudio(knocks[rand]);
         health -= damage;
         if(health - damage <= 0)
         {
             health = 0;
         }
+    }
+
+    void PlayAudio(AudioClip thisClip)
+    {
+        audioSource.clip = thisClip;
+        audioSource.Play();
     }
 }
